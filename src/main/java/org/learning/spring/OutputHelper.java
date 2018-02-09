fe2c76b57ee025308e82f30a6c36cb9e9f64a298
AnkithRD <rdankith@gmail.com>
2018-02-09 11:09:15 -0500

    OutputHelper class added

---
+++ src/main/java/org/learning/spring/OutputHelper.java
@@ -0,0 +1,14 @@
+package org.learning.spring;
+
+public class OutputHelper {
+
+	IOGenerator ioGenerator;
+
+	public IOGenerator getIoGenerator() {
+		return ioGenerator;
+	}
+
+	public void setIoGenerator(IOGenerator ioGenerator) {
+		this.ioGenerator = ioGenerator;
+	}	
+}
