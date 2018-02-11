465e848113d7c1483c960d78479e0938cc5caf40
AnkithRD <rdankith@gmail.com>
2018-02-11 15:56:45 -0500

    file name added by using fileNameGenerator class

--- src/main/java/org/learning/spring/CSVGenerator.java
+++ src/main/java/org/learning/spring/CSVGenerator.java
@@ -2,10 +2,20 @@ package org.learning.spring;
 
 public class CSVGenerator implements IOGenerator {
 
+	FileNameGenerator nameGen;
+
 	@Override
 	public void generateOutput() {
-		System.out.println("CSV File generated");
+		System.out.println("CSV File generated with the name " + nameGen.getFileName());
 
+	}
+
+	public FileNameGenerator getNameGen() {
+		return nameGen;
+	}
+
+	public void setNameGen(FileNameGenerator nameGen) {
+		this.nameGen = nameGen;
 	}
 
 }
--- src/main/java/org/learning/spring/JSONGenerator.java
+++ src/main/java/org/learning/spring/JSONGenerator.java
@@ -2,10 +2,20 @@ package org.learning.spring;
 
 public class JSONGenerator implements IOGenerator {
 
+	FileNameGenerator nameGen;
+	
 	@Override
 	public void generateOutput() {
-		System.out.println("JSON response generated");
+		System.out.println("JSON response generated with the name " + nameGen.getFileName());
 
+	}
+	
+	public FileNameGenerator getNameGen() {
+		return nameGen;
+	}
+
+	public void setNameGen(FileNameGenerator nameGen) {
+		this.nameGen = nameGen;
 	}
 
 }
